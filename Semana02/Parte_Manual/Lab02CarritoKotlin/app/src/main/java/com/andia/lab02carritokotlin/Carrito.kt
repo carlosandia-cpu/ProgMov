3a2be6dc03fe7d74948420844f476757a392f4d7
Carlos Andia <carlos.andia@tecsup.edu.pe>
2026-08-27 23:02:45 -0500

    Proyecto inicial con menú del carrito

---
+++ Semana02/Parte_Manual/Lab02CarritoKotlin/app/src/main/java/com/andia/lab02carritokotlin/Carrito.kt
@@ -0,0 +1,7 @@
+package com.andia.lab02carritokotlin
+
+fun main() {
+    println("=========================================")
+    println(" CARRITO DE COMPRAS - TIENDA TECSUP ")
+    println("=========================================")
+}
