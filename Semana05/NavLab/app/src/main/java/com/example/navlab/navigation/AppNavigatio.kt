d2c73daf9556331aaa34e3b4178027b2f14f0df8
Carlos Andia <carlos.andia@tecsup.edu.pe>
2026-09-22 23:56:18 -0500

    Semana 05: configurar Navigation Compose y definir rutas

---
+++ Semana05/NavLab/app/src/main/java/com/example/navlab/navigation/AppNavigatio.kt
@@ -0,0 +1,43 @@
+package com.example.navlab.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.navlab.screens.DetailScreen
+import com.example.navlab.screens.HomeScreen
+import com.example.navlab.screens.ListScreen
+
+@Composable
+fun AppNavigation() {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Home.route
+    ) {
+        composable(Screen.Home.route) {
+            HomeScreen(navController)
+        }
+
+        composable(Screen.List.route) {
+            ListScreen(navController)
+        }
+
+        composable(Screen.Profile.route) {
+            ProfileScreen(navController)
+        }
+
+        composable(
+            route = Screen.Detail.route,
+            arguments = listOf(
+                navArgument("itemId") { type = NavType.IntType }
+            )
+        ) { backStackEntry ->
+            val itemId = backStackEntry.arguments?.getInt("itemId") ?: 0
+            DetailScreen(navController, itemId)
+        }
+    }
+}
---
+++ Semana05/NavLab/app/src/main/java/com/example/navlab/screens/HomeScreen.kt
@@ -0,0 +1,4 @@
+package com.example.navlab.screens
+
+class HomeScreen {
+}
